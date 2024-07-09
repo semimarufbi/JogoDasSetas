b5be0dedd26e2d030efbaf08ca90271a950aab91
semimarufbi <gabicolzani@gmail.com>
2024-07-09 14:58:34 -0300

    tentamos
    tentamos  finalizar o metodo atualizarsetas

--- Jogo Das Setas/Assets/UIManager.cs
+++ Jogo Das Setas/Assets/UIManager.cs
@@ -1,18 +1,38 @@
+using JetBrains.Annotations;
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    UIManager instance;
+    #region singlenton
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    #endregion
+    [SerializeField]
+    public Image[] imagens;
+    [SerializeField]
+    public Sprite[] sprites;
+    [SerializeField]
+    TextMeshProUGUI textoDePontuacao,textoDoRelogio;
+    void atualizarSetas(KeyCode[] setas)
+    { 
+        for(int i = 0; i <imagens.Length; i++)
+        {
+            if (i>= setas.Length)
+            {
+                Image[i].sprite = sprites[0];  
+            }
+        }
     }
+                
+    
+    
+
 }
