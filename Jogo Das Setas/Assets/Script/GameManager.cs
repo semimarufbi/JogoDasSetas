7dc2579bbca26c3638b168c4301fcae68caa2013
RafaelRaiser <rafabarrosraiser@gmail.com>
2024-07-09 14:47:39 -0300

    Comit Atualização de void

---
+++ Jogo Das Setas/Assets/Script/GameManager.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int pontos, teclaAtual;
+    float relogio;
+    KeyCode[] teclas;
+
+    private void Start()
+    {
+        GerarSetas();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ChecarTecla(DownArrow);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+            ChecarTecla(UpArrow);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ChecarTecla(RightArrow);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ChecarTecla(LeftArrow);
+        }
+        ContagemRegressiva();
+    }
+}
